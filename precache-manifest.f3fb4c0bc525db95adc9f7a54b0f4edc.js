fa0134295cbda6ae76f41c013faca7d2897b9ee9
silvioaburto <sjortiz@ucdavis.edu>
2020-01-02 09:12:06 -0800

    Updates

---
+++ precache-manifest.f3fb4c0bc525db95adc9f7a54b0f4edc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e126e029de5cb1bf75db9a29c32a6205",
+    "url": "/react_js/index.html"
+  },
+  {
+    "revision": "4a5c21156d56576fddf5",
+    "url": "/react_js/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4212acc56508b9b22dc0",
+    "url": "/react_js/static/js/2.29e2b810.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react_js/static/js/2.29e2b810.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4a5c21156d56576fddf5",
+    "url": "/react_js/static/js/main.33d50e0f.chunk.js"
+  },
+  {
+    "revision": "ad9a9dba9bea0ee99697",
+    "url": "/react_js/static/js/runtime-main.05dbcbab.js"
+  }
+]);
